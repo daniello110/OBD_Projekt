2a14ab0bf767dec5493fff3b7090403298668206
Daniel Laskowski <daniello110@interia.pl>
2018-06-02 16:51:16 +0200

    Zaimportowanie bibliotek db4o do pliku

--- OBD_Projekt/Program.cs
+++ OBD_Projekt/Program.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Db4objects.Db4o;
+using Db4objects.Db4o.CS;
+using Db4objects.Db4o.Linq;
+using Db4objects.Db4o.NativeQueries;
+using Db4objects.Db4o.Instrumentation;
 
 namespace OBD_Projekt
 {
     class Program
     {
+        public static void ListResult(IObjectSet result)
+        {
+            Console.WriteLine(result.Count);
+            foreach (object item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            IObjectContainer db = Db4oEmbedded.OpenFile(Db4oEmbedded.NewConfiguration(), "PojazdyDB");
+            try
+            {
+
+            }
+            finally
+            {
+                db.Close();
+            }
+
         }
     }
 }
